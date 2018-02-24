d8a742a33eeb5d1225c239fbf77c4b24c0cc6004
Christian Rhodes <christian.rhodes@oit.edu>
2018-02-24 14:26:35 -0800

    first commit

---
+++ Lab6/scripts/app.js
@@ -0,0 +1,13 @@
+﻿function myFunction() {
+    document.getElementById("myForm").submit();
+
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var age = document.getElementById("age").value;
+
+    var info = firstName + "" + lastName + "" + age + "years old.";
+
+    document.getElementById("info").innerText = info;
+
+    return false;
+}
